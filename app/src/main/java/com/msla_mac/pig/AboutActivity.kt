dbae27792a9e7ce0300d7775793d47fa4f215728
Lillie McAlister <lilliemcalister1993@live.com>
2023-02-15 15:31:43 -0800

    added about page activity and layout

---
+++ app/src/main/java/com/msla_mac/pig/AboutActivity.kt
@@ -0,0 +1,25 @@
+package com.msla_mac.pig
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+
+    fun imageViewOnClick(vw: View){
+
+    }
+
+    fun btnSupportOnClick(vw: View){
+
+    }
+
+    fun btnAboutOnClick(vw: View){
+
+    }
+    
+}
