44e6cb27c1453542d66c2c8758f58d9595ee3af1
Lillie McAlister <lilliemcalister1993@live.com>
2023-02-15 18:44:12 -0800

    adding a support switch

--- app/src/main/java/com/msla_mac/pig/AboutActivity.kt
+++ app/src/main/java/com/msla_mac/pig/AboutActivity.kt
@@ -1,6 +1,7 @@
 package com.msla_mac.pig
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -19,7 +20,12 @@ class AboutActivity : AppCompatActivity() {
     }
 
     fun btnSupportOnClick(vw: View){
-
+        //website we kink to
+        val uri: Uri = Uri.parse("https://github.com/LA-McAlister")
+        //creating the switch logic
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        //calling the switch
+        startActivity(intent)
     }
 
     fun btnAboutOnClick(vw: View){
